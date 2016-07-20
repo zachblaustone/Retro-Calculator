283351a4f5a7e8ba7d138cbb184a43e1d3ed9079
ZachBlaustone <Zachblaustone@gmail.com>
2016-07-20 13:51:48 -0500

    Retro Cal Commit
    This is the first Commit of the RetroCal App

--- Retro-Calculator/ViewController.swift
+++ Retro-Calculator/ViewController.swift
@@ -7,19 +7,129 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Substract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    
+    var btnSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftvalStr = ""
+    var rightvalStr = ""
+    var currentOperation: Operation = Operation.Empty
+    var result = ""
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+// Play button sound
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+// Actions
+    
+    @IBAction func nummberPressed(btn: UIButton!) {
+        playSound()
+        
+        runningNumber += "\(btn.tag)"
+        outputLbl.text = runningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(Operation.Multiply)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onMinusPressed(sender: AnyObject) {
+        processOperation(Operation.Substract)
+    }
+    @IBAction func onAdditionPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
+    }
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(currentOperation)
+    }
+    
+    @IBAction func onClearPressed(sender: AnyObject) {
+        playSound()
+        restartCal()
+        
     }
 
-
+    func processOperation(op: Operation) {
+        playSound()
+        
+        if currentOperation != Operation.Empty {
+            
+            if runningNumber != "" {
+                rightvalStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftvalStr)! * Double(rightvalStr)!)"
+                    
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftvalStr)! + Double(rightvalStr)!)"
+                    
+                } else if currentOperation == Operation.Substract {
+                    result = "\(Double(leftvalStr)! - Double(rightvalStr)!)"
+                    
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftvalStr)! / Double(rightvalStr)!)"
+                }
+            
+            }
+    
+            leftvalStr = result
+            outputLbl.text = result
+            currentOperation = op
+            
+        } else {
+            leftvalStr = runningNumber
+            runningNumber = ""
+            currentOperation = op
+        }
+    }
+    
+    func playSound() {
+        if btnSound.playing {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
+    
+    func restartCal() {
+        outputLbl.text = "0"
+        leftvalStr = ""
+        rightvalStr = ""
+        runningNumber = ""
+        currentOperation = Operation.Empty
+    }
 }
 
